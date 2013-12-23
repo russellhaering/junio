059e1086e65533b828201e694aac60317c53e4dc
Russell Haering <russell.haering@rackspace.com>
2013-12-22 21:32:56 -0800

    stub out beginnings of queue type

--- server/server.go
+++ server/server.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+type JunioQueue struct {
+	Project string
+	Name string
+	NextIndex int
+}
+
+func NewJunioQueue() JunioQueue {
+	return JunioQueue{}
+}
+
 func main() {
 	fmt.Println("starting junio server")
 }
